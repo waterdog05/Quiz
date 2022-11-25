245edb6e184ad02bdb08e66a940cec457803b78e
waterdog05 <84177301+waterdog05@users.noreply.github.com>
2022-11-26 07:07:14 +0900

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -24,26 +23,3 @@ Quiz.prototype.guess = function(answer) {
 Quiz.prototype.isEnded = function() {
     return this.questionIndex === this.questions.length;
 }
-=======
-function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0;
-}
-
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions[this.questionIndex];
-}
-
-Quiz.prototype.guess = function(answer) {
-    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-        this.score++;
-    }
-
-    this.questionIndex++;
-}
-
-Quiz.prototype.isEnded = function() {
-    return this.questionIndex === this.questions.length;
-}
->>>>>>> 34d0f7dfcd0ef821a1ac7c1904c735fff67477cd
